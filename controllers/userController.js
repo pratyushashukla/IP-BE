fcd09f6e70920a68746f5a22b14c7cb0874afad6
Bluwzy <aliarif1999@gmail.com>
2024-10-16 18:18:01 -0400

    fetch user info for profile page is added

--- controllers/userController.js
+++ controllers/userController.js
@@ -9,3 +9,18 @@ export const getUsers = async (req, res) => {
     res.status(500).json({ message: "Error fetching users", error });
   }
 };
+
+// Get a user by ID
+export const getUserById = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    res.status(200).json({ user });
+  } catch (error) {
+    res.status(500).json({ message: "Error fetching user", error });
+  }
+};
