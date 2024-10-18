5f059767c93cefa1277f46f3c41d5102bcdb3e4f
Srijesh Khanal <khanal.srijesh@gmail.com>
2024-10-18 11:07:08 -0400

    Added APIs to update and delete users

--- controllers/userController.js
+++ controllers/userController.js
@@ -9,3 +9,36 @@ export const getUsers = async (req, res) => {
     res.status(500).json({ message: "Error fetching users", error });
   }
 };
+
+
+// Update a user
+export const updateUser = async (req, res) => {
+  try {
+    const { firstname, lastname, email, phone, address } = req.body;
+
+    const user = await User.findByIdAndUpdate(req.params.id, { firstname, lastname, email, phone, address }, { new: true });
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    res.status(200).json({ message: "User updated successfully", user });
+  } catch (error) {
+    res.status(500).json({ message: "Error updating user", error });
+  }
+};
+
+// Delete a user. We dont give feature to delete user. It can be used for internal use
+export const deleteUser = async (req, res) => {
+  try {
+    const user = await User.findByIdAndDelete(req.params.id);
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    res.status(200).json({ message: "User deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ message: "Error deleting user", error });
+  }
+};
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -5,4 +5,11 @@ const router = express.Router();
 // Get all users
 router.get("/", userController.getUsers);
 
+// Update a user
+router.patch("/:id", userController.updateUser);
+
+// Delete a user
+router.delete("/:id", userController.deleteUser);
+
+
 export default router;
