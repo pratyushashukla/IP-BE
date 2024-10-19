0950aae77a3af25e4bcbd317141a5b1e852fde9d
Srijesh Khanal <khanal.srijesh@gmail.com>
2024-10-18 22:26:47 -0400

    Added missing routes

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -2,8 +2,14 @@ import express from "express";
 import * as userController from "../controllers/userController.js";
 const router = express.Router();
 
+// Create a new user
+router.post("/", userController.createUser);
+
 // Get all users
 router.get("/", userController.getUsers);
+
+// Get a user by ID
+router.get("/:id", userController.getUserById);
 
 // Update a user
 router.patch("/:id", userController.updateUser);
