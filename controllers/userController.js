3a36996b2ba6281e4c4484a2b176f49eb416867c
Srijesh Khanal <khanal.srijesh@gmail.com>
2024-10-18 22:09:54 -0400

    Merged User CRUD APIs to resolve conflict

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,18 @@
 import User from "../models/userModel.js";
 
+// Create a new user
+export const createUser = async (req, res) => {
+  try {
+
+    const user = new User(req.body.data);
+
+    await user.save();
+    res.status(201).json({ message: "User created successfully", user });
+  } catch (error) {
+    res.status(500).json({ message: "Error creating user", error });
+  }
+};
+
 // Get all users
 export const getUsers = async (req, res) => {
   try {
@@ -7,6 +20,21 @@ export const getUsers = async (req, res) => {
     res.status(200).json({ users });
   } catch (error) {
     res.status(500).json({ message: "Error fetching users", error });
+  }
+};
+
+// Get a user by ID
+export const getUserById = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    res.status(200).json({ user });
+  } catch (error) {
+    res.status(500).json({ message: "Error fetching user", error });
   }
 };
 
