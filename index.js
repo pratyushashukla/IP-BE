a109a34da35bced841464e4d75fda9d1ea77ef20
pratyushashukla <pratyusha.shukla212@gmail.com>
2024-11-11 22:30:09 -0500

    fixed jwt expired - auto logout

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ import tasksRoute from "./routes/tasksRoute.js";
 import inmateRoute from "./routes/inmateRoute.js";
 import visitorRoute from "./routes/visitorRoute.js";
 import appointmentsRoute from "./routes/appointmentsRoute.js";
-import mealRoute from './routes/mealRoute.js';
-import allergyRoute from './routes/allergyRoute.js';
+import mealRoute from "./routes/mealRoute.js";
+import allergyRoute from "./routes/allergyRoute.js";
 
 import User from "./models/userModel.js";
 import { connect } from "./config/dbconnection.js";
@@ -71,9 +71,12 @@ const handleAuthToken = async (req, res, next) => {
   const result = auth.verifyToken(authToken);
 
   if (!result?.status) {
-    return res
-      .status(401)
-      .json({ message: result.message ? result.message : "Invalid Token" });
+    // Clear the authentication cookie
+    res.clearCookie("authtoken");
+    // Send 401 response with a message instructing the client to redirect
+    return res.status(401).json({
+      message: result?.message || "Invalid Token. Redirecting to sign-in.",
+    });
   }
 
   try {
@@ -121,8 +124,8 @@ app.use("/api/v1/tasks", tasksRoute);
 app.use("/api/v1/inmates", inmateRoute);
 app.use("/api/v1/visitors", visitorRoute);
 app.use("/api/v1/appointments", appointmentsRoute);
-app.use('/api/v1/meals', mealRoute);
-app.use('/api/v1/allergies', allergyRoute);
+app.use("/api/v1/meals", mealRoute);
+app.use("/api/v1/allergies", allergyRoute);
 
 //create connection
 connect();
